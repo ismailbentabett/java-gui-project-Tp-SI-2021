f43ab429f07875049499e3407b9ad02337bf1a36
ismail bentabet <xisben2001x@gmail.com>
2021-02-02 15:45:50 +0100

    offffffffff

---
+++ src/FileStorage.java
@@ -0,0 +1,138 @@
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FileStorage {
+
+    public static void writeNewFile(String fileName, String[] headers) throws IOException {
+        FileWriter fw = new FileWriter(fileName + ".txt");
+
+        for (String header : headers) {
+            fw.append(header);
+            if (! headers[headers.length - 1 ].equals(header)){
+                fw.append(",");
+            }else fw.append("\n");
+        }
+        fw.flush();
+        fw.close();
+    }
+
+    public static void addRecord(String fileName, String[] fields) throws IOException {
+        FileWriter fw = new FileWriter(fileName + ".txt", true);
+
+        for (String field : fields) {
+            fw.append(field);
+            if (! fields[fields.length - 1 ].equals(field)){
+                fw.append(",");
+            }else fw.append("\n");
+        }
+        fw.flush();
+        fw.close();
+    }
+
+    public static ArrayList<String[]> readData(String fileName) throws IOException {
+        BufferedReader fr = new BufferedReader(new FileReader(fileName + ".txt"));
+        ArrayList<String[]> data = new ArrayList<>();
+        String row = "";
+        while ((row = fr.readLine()) != null) {
+
+            String[] rowdata = row.split(",");
+            data.add(rowdata);
+        }
+        fr.close();
+        return data;
+    }
+
+    public static String getLastId(String fileName) throws IOException {
+        ArrayList<String[]> data = readData(fileName);
+        return data.get(data.size() - 1)[0];
+    }
+
+    public static boolean deleteRecord(String fileName, String id) throws IOException{
+        File inputFile = new File(fileName + ".txt");
+        File tempFile = new File("temp.txt");
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+        String lineToRemove = "bbb";
+        String currentLine;
+
+        while((currentLine = reader.readLine()) != null) {
+            String[] data = currentLine.split(",");
+            if (data[0].equals(id)){
+                lineToRemove = currentLine;
+                String trimmedLine = currentLine.trim();
+                if(trimmedLine.equals(lineToRemove)) continue;
+            }
+            writer.write(currentLine + System.getProperty("line.separator"));
+        }
+        writer.close();
+        reader.close();
+        if (inputFile.delete()){
+            tempFile.renameTo(new File(fileName + ".txt"));
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+    public static boolean updateRecord(String fileName, String id, String[] updatedRows) throws IOException{
+        File inputFile = new File(fileName + ".txt");
+        File tempFile = new File("temp.txt");
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+        String currentLine;
+
+        while((currentLine = reader.readLine()) != null) {
+            String[] data = currentLine.split(",");
+            if (data[0].equals(id)){
+                currentLine = Arrays.toString(updatedRows);
+                currentLine = currentLine.substring(1, currentLine.length() - 1);
+                currentLine = currentLine.replaceAll("\\s+", "");
+            }
+            writer.write(currentLine + System.getProperty("line.separator"));
+        }
+        writer.close();
+        reader.close();
+        if (inputFile.delete()){
+            tempFile.renameTo(new File(fileName + ".txt"));
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+    public static boolean updateTrip(String fileName, String id,String date,String time, String[] updatedRows) throws IOException{
+        File inputFile = new File(fileName + ".txt");
+        File tempFile = new File("temp.txt");
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+        String currentLine;
+
+        while((currentLine = reader.readLine()) != null) {
+            String[] data = currentLine.split(",");
+            if (data[0].equals(id) && data[1].equals(date) && data[2].equals(time)){
+                currentLine = Arrays.toString(updatedRows);
+                currentLine = currentLine.substring(1, currentLine.length() - 1);
+                currentLine = currentLine.replaceAll("\\s+", "");
+            }
+            writer.write(currentLine + System.getProperty("line.separator"));
+        }
+        writer.close();
+        reader.close();
+        if (inputFile.delete()){
+            tempFile.renameTo(new File(fileName + ".txt"));
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+}
--- src/GUI.java
+++ src/GUI.java
@@ -3,10 +3,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Stack;
@@ -15,10 +24,10 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumnModel;
 
-
 public class GUI {
 
 	public GUI() {
+
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int height = (int) screenSize.getHeight();
 
@@ -27,11 +36,9 @@ public class GUI {
 
 		frame = new JFrame();
 
-		JPanel NORTH_PANEL = new JPanel( new GridLayout(1,2));
-		
-		JLabel fullNamelabel = null ;
-	
-		
+		JPanel NORTH_PANEL = new JPanel(new GridLayout(1, 2));
+
+		JLabel fullNamelabel = null;
 
 		JPanel SOUTH_PANEL = new JPanel();
 
@@ -44,14 +51,13 @@ public class GUI {
 		JPanel bottom = new JPanel();
 
 		JSplitPane vertical = new JSplitPane(JSplitPane.VERTICAL_SPLIT, top, bottom);
-		//Dimension CENTER_PANELsize = CENTER_PANEL.getSize();
-		//vertical.setDividerLocation(0.5);
+		// Dimension CENTER_PANELsize = CENTER_PANEL.getSize();
+		// vertical.setDividerLocation(0.5);
 
-		//vertical.setSize(CENTER_PANELsize.height, CENTER_PANELsize.width);
+		// vertical.setSize(CENTER_PANELsize.height, CENTER_PANELsize.width);
 		vertical.setBackground(Color.black);
 		CENTER_PANEL.add(vertical, BorderLayout.CENTER);
 		FactureObj newfacture = new FactureObj();
-
 
 		JTextField firstName = new JTextField();
 		JTextField lastName = new JTextField();
@@ -65,34 +71,36 @@ public class GUI {
 		if (option == JOptionPane.OK_OPTION) {
 			String firstNameValue = firstName.getText();
 			String lastNameValue = lastName.getText();
-			fullNamelabel = new JLabel(firstNameValue +" " + lastNameValue);
-			newfacture.name = firstNameValue +" " + lastNameValue;
+			fullNamelabel = new JLabel(firstNameValue + " " + lastNameValue);
+			newfacture.name = firstNameValue + " " + lastNameValue;
 			;
 
 		}
-		
+
 		ProductsObj newproducts1 = new ProductsObj();
 		ProductsObj newproducts2 = new ProductsObj();
 		ProductsObj newproducts3 = new ProductsObj();
-		JLabel enterprize = new JLabel("Entreprise National Des Pièces") ;
-		JLabel zonename = new JLabel("15 Zone industrielle Rouiba\r\n" + "Algerie") ;
-		JLabel datenow = new JLabel(newfacture.now) ;
+		JLabel enterprize = new JLabel("Entreprise National Des Pièces");
+		JLabel zonename = new JLabel("15 Zone industrielle Rouiba\r\n" + "Algerie");
+		JLabel datenow = new JLabel(newfacture.now);
+
 		NORTH_PANEL.add(enterprize);
 		NORTH_PANEL.add(zonename);
 		NORTH_PANEL.add(datenow);
+
 		frame.add(NORTH_PANEL, BorderLayout.NORTH);
 		frame.add(SOUTH_PANEL, BorderLayout.SOUTH);
 		frame.add(EAST_PANEL, BorderLayout.EAST);
 		frame.add(WEST_PANEL, BorderLayout.WEST);
 		frame.add(CENTER_PANEL, BorderLayout.CENTER);
-		NORTH_PANEL.setBackground(Color.red);
-		SOUTH_PANEL.setBackground(Color.blue);
+		NORTH_PANEL.setBackground(Color.decode("#ff9292"));
+		SOUTH_PANEL.setBackground(Color.decode("#ff9292"));
 		;
-		EAST_PANEL.setBackground(Color.green);
+		EAST_PANEL.setBackground(Color.decode("#ffb4b4"));
 		;
-		WEST_PANEL.setBackground(Color.yellow);
+		WEST_PANEL.setBackground(Color.decode("#ffb4b4"));
 		;
-		CENTER_PANEL.setBackground(Color.black);
+		CENTER_PANEL.setBackground(Color.decode("#ffdcdc"));
 		;
 		NORTH_PANEL.setPreferredSize(new Dimension(width, 100));
 		SOUTH_PANEL.setPreferredSize(new Dimension(width, 100));
@@ -123,6 +131,7 @@ public class GUI {
 		DefaultTableModel tableModelfac = new DefaultTableModel(colfac, 0);
 
 		JTable tablefac = new JTable(tableModelfac);
+
 		addProduct.addActionListener(new ActionListener() {
 			int productChecker = 0;
 
@@ -199,71 +208,75 @@ public class GUI {
 		});
 		TableColumnModel columnModel = table.getColumnModel();
 		TableColumnModel columnModelfac = tablefac.getColumnModel();
-		columnModel.getColumn(0).setPreferredWidth(170);
-		columnModel.getColumn(1).setPreferredWidth(170);
-		columnModel.getColumn(2).setPreferredWidth(170);
-		columnModelfac.getColumn(0).setPreferredWidth(150);
-		columnModelfac.getColumn(1).setPreferredWidth(150);
-	
-		table.setRowHeight(150);
-		tablefac.setRowHeight(150);
+		
+		  columnModel.getColumn(0).setPreferredWidth(500);
+		  columnModel.getColumn(1).setPreferredWidth(500);
+		  columnModel.getColumn(2).setPreferredWidth(500);
+		  columnModel.getColumn(3).setPreferredWidth(500);
+		  columnModelfac.getColumn(0).setPreferredWidth(500);
+		  columnModelfac.getColumn(1).setPreferredWidth(500);
+		 
+
+		table.setRowHeight(250);
+		tablefac.setRowHeight(250);
 		top.add(new JScrollPane(table), BorderLayout.NORTH);
 		bottom.add(new JScrollPane(tablefac), BorderLayout.SOUTH);
-		NORTH_PANEL.add(fullNamelabel) ;
-	
-		frame.setVisible(true);
-		
-		// Our example data
-		frame.addWindowListener(new WindowAdapter() {
-		    public void windowClosing(WindowEvent e) {
-		        // call terminate
-				writeDataLineByLine("/",newproducts1,newproducts2,newproducts3,newfacture);
+		NORTH_PANEL.add(fullNamelabel);
+		JLabel facid = new JLabel(String.valueOf(count("output.txt")));
 
-		    }
+		NORTH_PANEL.add(facid);
+		frame.setVisible(true);
+
+		frame.addWindowListener(new WindowAdapter() {
+
+			public void windowClosing(WindowEvent e) {
+				// call terminate
+
+				// Our example data
+				String[] headercsv = { "id", "client name", "product 1", "product 2 price", "product 2 quantity",
+						"product 2", "product 2 price", "product 2 quantity", "product 2", "product 2 price",
+						"product 2 quantity", "total", };
+				try {
+					newfacture.id_number = count("output.txt");
+					System.out.print("id  " + FileStorage.getLastId("output"));
+					String[] list = { String.valueOf(newfacture.id_number), newfacture.name, newproducts1.productName,
+							String.valueOf(newproducts1.prix), String.valueOf(newproducts1.quantity),
+							newproducts2.productName, String.valueOf(newproducts2.prix),
+							String.valueOf(newproducts2.quantity), newproducts3.productName,
+							String.valueOf(newproducts2.prix), String.valueOf(newproducts2.quantity),
+							String.valueOf(newfacture.total),
+
+					};
+					FileStorage.addRecord("output", list);
+
+				} catch (IOException e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				}
+
+			}
 		});
 
-	
 	}
-	
-	public static void writeDataLineByLine(String filePath , ProductsObj newproducts1 , ProductsObj newproducts2 , ProductsObj newproducts3 , FactureObj newfacture) 
-	{ 
-	    // first create file object for file placed at location 
-	    // specified by filepath 
-		 try (PrintWriter writer = new PrintWriter(new File("test.csv"))) {
 
-		      StringBuilder sb1 = new StringBuilder();
-		      StringBuilder sb2 = new StringBuilder();
-		      StringBuilder sb3 = new StringBuilder();
-		      sb1.append(newfacture.name);
-		      sb1.append(newproducts1.productName);
-		      sb1.append(newproducts1.prix);
-		      sb1.append(newproducts1.quantity);
-		      sb1.append(newproducts1.ref);
+	public static int count(String fileName) {
 
-		      sb2.append(newfacture.name);
-		      sb2.append(newproducts2.productName);
-		      sb2.append(newproducts2.prix);
-		      sb2.append(newproducts2.quantity);
-		      sb2.append(newproducts2.ref);
-		      
-		      sb3.append(newfacture.name);
-		      sb3.append(newproducts3.productName);
-		      sb3.append(newproducts3.prix);
-		      sb3.append(newproducts3.quantity);
-		      sb3.append(newproducts3.ref);
-		
+		Path path = Paths.get(fileName);
 
-		      writer.write(sb1.toString());
-		      writer.write(sb2.toString());
-		      writer.write(sb3.toString());
+		long lines = 0;
+		try {
 
-		      System.out.println("done!");
+			// much slower, this task better with sequence access
+			// lines = Files.lines(path).parallel().count();
 
-		    } catch (FileNotFoundException e) {
-		      System.out.println(e.getMessage());
-		    }
-	} 
-	
-	
+			lines = Files.lines(path).count();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return (int) lines;
+
+	}
 
 }
